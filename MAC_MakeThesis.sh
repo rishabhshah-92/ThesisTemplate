c55d27c5d372ddce80b83fda0e6fda4db9ca4218
rishabhshah-92 <rshah6192@gmail.com>
2020-09-01 16:54:44 -0700

    Commit2
    Second chapter, a user manual, and auto-cleanup after code compilation

--- MAC_MakeThesis.sh
+++ MAC_MakeThesis.sh
@@ -3,12 +3,55 @@ cd "$(dirname "$0")"
 cd AllOtherCrap
 
 pdflatex Thesis_Central.tex
+
+
+
+######################################
+### Chapter-specific compilations ####
+
 bibtex ch01/ch01.tex
 bibtex ch01/ch01.aux
+bibtex ch02/ch02.tex
+bibtex ch02/ch02.aux
+
+
+
+######################################
+##### Entire thesis compilation ######
+
 bibtex Thesis_Central.tex
 pdflatex Thesis_Central.tex
 pdflatex Thesis_Central.tex
 pdflatex Thesis_Central.tex
+
+
+
+######################################
+############# Clean up ###############
+
+## The following block of code cleans 
+## up the auxiliary files generated 
+## during compilation
+rm Abstract.aux
+rm Thanks.aux
+rm Thesis_Central.aux
+rm Thesis_Central.lof
+rm Thesis_Central.log
+rm Thesis_Central.lot
+rm Thesis_Central.out
+rm Thesis_Central.toc
+rm TitleAndCopyright.aux
+
+##Chapter-specific cleanup
+rm ch01/ch01.aux
+rm ch01/ch01.bbl
+rm ch01/ch01.blg
+rm ch02/ch02.aux
+rm ch02/ch02.bbl
+rm ch02/ch02.blg
+
+######################################
+### Move Thesis to root folder #######
 
 mv Thesis_Central.pdf ../MyThesis.pdf
 open ../MyThesis.pdf
--- WIN_MakeThesis.bat
+++ WIN_MakeThesis.bat
@@ -1,12 +1,53 @@
 cd %cd%\AllOtherCrap
 
 pdflatex Thesis_Central.tex
+
+rem ######################################
+rem ### Chapter-specific compilations ####
+
 bibtex ch01/ch01.tex
 bibtex ch01/ch01.aux
+bibtex ch02/ch02.tex
+bibtex ch02/ch02.aux
+
+
+
+rem ######################################
+rem ##### Entire thesis compilation ######
+
 bibtex Thesis_Central.tex
 pdflatex Thesis_Central.tex
 pdflatex Thesis_Central.tex
 pdflatex Thesis_Central.tex
+
+
+rem ######################################
+rem ############# Clean up ###############
+
+rem ## The following block of code cleans 
+rem ## up the auxiliary files generated 
+rem ## during compilation
+
+del /f Abstract.aux
+del /f Thanks.aux
+del /f Thesis_Central.aux
+del /f Thesis_Central.lof
+del /f Thesis_Central.log
+del /f Thesis_Central.lot
+del /f Thesis_Central.out
+del /f Thesis_Central.toc
+del /f TitleAndCopyright.aux
+
+rem ##Chapter-specific cleanup
+del /f ch01/ch01.aux
+del /f ch01/ch01.bbl
+del /f ch01/ch01.blg
+del /f ch02/ch02.aux
+del /f ch02/ch02.bbl
+del /f ch02/ch02.blg
+
+rem ######################################
+rem ### Move Thesis to root folder #######
 
 move Thesis_Central.pdf ..\MyThesis.pdf
 start ..\MyThesis.pdf
